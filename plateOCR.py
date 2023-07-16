0497924498127973a6e95723c6622483cac5d8b6
TerenceGrover <terencegrover@gmail.com>
2023-07-16 23:20:38 +0200

    feat : plateOCR & coordinates delimitation

---
+++ plateOCR.py
@@ -0,0 +1,39 @@
+# import matplotlib.pyplot as plt
+import cv2
+import easyocr
+from IPython.display import Image
+import urllib.request
+import ssl
+import matplotlib.pyplot as plt
+
+
+def coordinates(img):
+  reader = easyocr.Reader(['en'])
+  # Turn link into readable image
+  output = reader.readtext(img)
+  cord = output[-1][0]
+  x_min, y_min = [int(min(idx)) for idx in zip(*cord)]
+  x_max, y_max = [int(max(idx)) for idx in zip(*cord)]
+  image = cv2.imread(img)
+  plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+
+  return x_min, y_min, x_max, y_max
+
+def download_image(url, filename):
+    context = ssl._create_unverified_context()
+
+    # Create a request object with the desired 'User-Agent' header
+    req = urllib.request.Request(
+        url,
+        data=None,
+        headers={
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'
+        }
+    )
+
+    with urllib.request.urlopen(req, context=context) as u, open('temp_img/'+filename, 'wb') as f:
+        f.write(u.read())
+
+# Now you can use this function to download the image:
+# download_image('https://img03.platesmania.com/230715/o/22063239.jpg','test.jpg')
+print(coordinates('temp_img/test.jpg'))
